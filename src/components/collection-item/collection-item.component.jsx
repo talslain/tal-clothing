32faba5ae94c1292025b840a4bcb47ffb393ec14
Matt <therealtalslain@gmail.com>
2020-04-25 12:00:22 +0100

    i dont even know what I did here

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -4,15 +4,17 @@ import "./collection-item.styles.scss";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
     <div className="collection-item">
-        <div 
-            className="image"
-            style={{
-                backgroundImage: `url(${imageUrl})`
+        <div className="header">
+            <div 
+                className="background-image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`
             }} />
             <div className="collection-footer">
-            <span className="name">{name}</span>
-            <span className="price">{price}</span>
+                <span className="name">{name}</span>
+                <span className="price">{price}</span>
             </div>
+        </div>
     </div>
 );
 
